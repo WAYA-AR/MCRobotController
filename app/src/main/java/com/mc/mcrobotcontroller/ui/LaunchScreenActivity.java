e7866cad1ee67e900f43b99e3233aeb7b7329398
mcharfi <meriam.charfi.p@gmail.com>
2018-03-31 22:21:20 +0200

    <DEV>: LaunchScreen permission request

--- app/src/main/java/com/mc/mcrobotcontroller/ui/LaunchScreenActivity.java
+++ app/src/main/java/com/mc/mcrobotcontroller/ui/LaunchScreenActivity.java
@@ -1,30 +1,65 @@
 package com.mc.mcrobotcontroller.ui;
 
+import android.Manifest;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 
 import com.mc.mcrobotcontroller.R;
+import com.mc.mcrobotcontroller.utils.PermissionUtils;
+
+import java.util.ArrayList;
 
 public class LaunchScreenActivity extends AppCompatActivity {
+
+    private PermissionUtils mPermissionUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
 
+        ArrayList<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.BLUETOOTH);
+        permissions.add(Manifest.permission.BLUETOOTH_ADMIN);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        mPermissionUtils = new PermissionUtils(this, permissions, new PermissionUtils.OnPermissionGrantedHandler() {
+            @Override
+            public void onPermissionsGranted() {
+                goToNextScreen();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        startActivity(new Intent(LaunchScreenActivity.this, DeviceSelectionActivity.class));
+
+                        mPermissionUtils.askPermissions();
                     }
                 });
             }
         }, 1000);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mPermissionUtils.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    void goToNextScreen(){
+        startActivity(new Intent(LaunchScreenActivity.this, DeviceSelectionActivity.class));
+    }
+
 }
