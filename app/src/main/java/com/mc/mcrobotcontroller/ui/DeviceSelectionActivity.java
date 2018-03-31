c909795be17e69698e345ade38d3a1e8542fab0d
mcharfi <meriam.charfi.p@gmail.com>
2018-03-31 13:54:21 +0200

    <DEV>: Device selection activity creation

---
+++ app/src/main/java/com/mc/mcrobotcontroller/ui/DeviceSelectionActivity.java
@@ -0,0 +1,19 @@
+package com.mc.mcrobotcontroller.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mc.mcrobotcontroller.R;
+
+/**
+ * Created by Meriam on 31/03/2018.
+ */
+
+public class DeviceSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_selection);
+    }
+}
--- app/src/main/java/com/mc/mcrobotcontroller/ui/LaunchScreenActivity.java
+++ app/src/main/java/com/mc/mcrobotcontroller/ui/LaunchScreenActivity.java
@@ -1,5 +1,7 @@
 package com.mc.mcrobotcontroller.ui;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -12,5 +14,17 @@ public class LaunchScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(LaunchScreenActivity.this, DeviceSelectionActivity.class));
+                    }
+                });
+            }
+        }, 1000);
     }
 }
