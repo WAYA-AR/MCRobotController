e1d1156b0df04a21ef39adb3b3ad0d309a81146d
mcharfi <meriam.charfi.p@gmail.com>
2018-03-31 22:21:37 +0200

    <DEV>: bluethooth scan

---
+++ app/src/main/java/com/mc/mcrobotcontroller/delegate/DeviceSelectionDelegate.java
@@ -0,0 +1,94 @@
+package com.mc.mcrobotcontroller.delegate;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.aflak.bluetooth.Bluetooth;
+import me.aflak.bluetooth.DiscoveryCallback;
+
+/**
+ * Created by mcharfi on 31/03/2018.
+ */
+
+public class DeviceSelectionDelegate implements DiscoveryCallback {
+    private Bluetooth mBluetooth;
+    private Context mContext;
+    private List<BluetoothDevice> mDevices = new ArrayList<>();
+
+    private OnScanListener mOnScanListener;
+
+    public DeviceSelectionDelegate(Context context, OnScanListener onScanListener){
+        mContext = context;
+        mOnScanListener = onScanListener;
+        mBluetooth = new Bluetooth(mContext);
+        mBluetooth.setDiscoveryCallback(this);
+    }
+
+
+    //
+    public void scanDevices(){
+        if (mOnScanListener == null){
+            return;
+        }
+
+        if(!mBluetooth.isEnabled())
+            mBluetooth.enable();
+
+        mBluetooth.startScanning();
+    }
+
+    public void onStart(){
+        mBluetooth.onStart();
+    }
+
+    public void onStop(){
+        if (mBluetooth != null || mBluetooth.isEnabled())
+        mBluetooth.onStop();
+    }
+    //DiscoveryCallback
+    @Override
+    public void onDiscoveryStarted() {
+        Log.d(this.getClass().getCanonicalName(),"onDiscoveryStarted");
+        mDevices.clear();
+    }
+
+    @Override
+    public void onDiscoveryFinished() {
+        Log.d(this.getClass().getCanonicalName(),"onDiscoveryFinished");
+        for (BluetoothDevice device: mDevices){
+            String tmp = device.getName() +  " " + device.getAddress();
+            Log.d(this.getClass().getCanonicalName(),tmp);
+        }
+    }
+
+    @Override
+    public void onDeviceFound(BluetoothDevice device) {
+        Log.d(this.getClass().getCanonicalName(),"onDeviceFound");
+        mDevices.add(device);
+
+    }
+
+    @Override
+    public void onDevicePaired(BluetoothDevice device) {
+
+    }
+
+    @Override
+    public void onDeviceUnpaired(BluetoothDevice device) {
+
+    }
+
+    @Override
+    public void onError(String message) {
+        Log.e(this.getClass().getCanonicalName(),"onError: " + message);
+
+    }
+
+    public interface OnScanListener{
+       void onScannedDevices(List<BluetoothDevice> devices);
+    }
+}
--- app/src/main/java/com/mc/mcrobotcontroller/ui/DeviceSelectionActivity.java
+++ app/src/main/java/com/mc/mcrobotcontroller/ui/DeviceSelectionActivity.java
@@ -1,19 +1,75 @@
 package com.mc.mcrobotcontroller.ui;
 
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mc.mcrobotcontroller.R;
+import com.mc.mcrobotcontroller.delegate.DeviceSelectionDelegate;
+
+import java.util.List;
 
 /**
- * Created by Meriam on 31/03/2018.
+ * Created by mcharfi on 31/03/2018.
  */
 
-public class DeviceSelectionActivity extends AppCompatActivity {
+public class DeviceSelectionActivity extends AppCompatActivity implements DeviceSelectionDelegate.OnScanListener{
+
+    private DeviceSelectionDelegate mDelegate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_selection);
+
+        mDelegate = new DeviceSelectionDelegate(this, this);
+    }
+
+    @Override protected void onStart() {
+        super.onStart();
+        mDelegate.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDelegate.scanDevices();
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        mDelegate.onStop();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Quitter?")
+                .setMessage("Etes-vous sûr de vouloir quitter l'application?")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                            finishAffinity();
+                        }else{
+                            finish();
+                        }
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .create();
+        dialog.show();
+    }
+
+    @Override
+    public void onScannedDevices(List<BluetoothDevice> devices) {
+
     }
 }
